e03bbc3cdd5ab2a5f376cc2311672ed9ee4f495c
Brian King <free.as.in.speech@gmail.com>
2015-07-29 17:24:04 +0200

    starting to add tabbar controller. hooked up to sent list view, hides tabbar when going to editor

--- imagepicker/SentViewController.swift
+++ imagepicker/SentViewController.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class SentViewController: UIViewController {
-	
+
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if let identifier = segue.identifier {
+			if identifier == "fromListToEditor" {
+				// Hide the tab bar on the current controller's view before pushing, so that there's not an ugly animation
+				// of hiding the tab bar after the destination VC has appeared.
+				// The destination view controller has a storyboard property that hides the tabbar for it's view.
+				self.tabBarController!.tabBar.hidden = true
+			}
+		}
+	}
+
 }
